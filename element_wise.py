2172651cc3f31c11b97a4459d1a2b853fbaad0ac
cutty12 <cuttyswiss@protonmail.com>
2021-02-03 15:09:36 +0100

    added new file element-wise

---
+++ element_wise.py
@@ -0,0 +1,9 @@
+"""NumPy element-wise logical operations
+
+NumPy Arrays support element-wise logical operations, returning new Arrays populated with False or True based on their evaluation.
+
+"""
+numbers = np.array([-5,4,0,2,3])
+is_positive = numbers > 0
+print(is_positive)
+
--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+"""NumPy elements can be indexed using conditionals. The syntax to filter an array using a conditional is array_name[conditional].
+
+The returned array will contain only the elements for which the conditional evaluates to True.
+"""
+
 import numpy as np
 
 numbers = np.array([-5,4,0,2,3])
